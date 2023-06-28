f3c57440c1400c7f3ec3f741e9f05c9ca4d2073f
Saikumar Konakanchi <120327792+SaikumarKonakanchi0@users.noreply.github.com>
2023-06-28 19:12:28 +0530

    Update count_ev_od.py

--- count_ev_od.py
+++ count_ev_od.py
@@ -8,3 +8,16 @@ for i in n:
         od_count+=1
 print("Number of even numbers: ",ev_count)
 print("Number of odd numbers: ",od_count)
+
+
+#Below is the same code but input is taken from the user
+n=int(input())
+ev_count=0
+od_count=0
+for i in range(1,n+1):
+    if i%2==0:
+        ev_count+=1
+    else:
+        od_count+=1
+print("Number of even numbers: ",ev_count)
+print("Number of odd numbers: ",od_count)
